2acf659e46735ca5640cb49ad494e13428c6c3a3
EJ Rainville <erainvil@uw.edu>
2021-08-27 11:42:24 -0700

    Updated some comments in the main microSWIFT.py script

--- microSWIFT.py
+++ microSWIFT.py
@@ -51,7 +51,6 @@ from utils.config3 import Config
 # Main body of microSWIFT.py
 if __name__=="__main__":
 
-    # ------------ Logging Characteristics ---------------
     # Define Config file name
     configFilename = r'utils/Config.dat'
     config = Config() # Create object and load file
@@ -103,7 +102,7 @@ if __name__=="__main__":
     logger.info('microSWIFT configuration:')
     logger.info('float ID: {0}, payload type: {1}, sensors type: {2}, '.format(floatID, payload_type, sensor_type))
     logger.info('burst seconds: {0}, burst interval: {1}, burst time: {2}'.format(burst_seconds, burst_int, burst_time))
-    # logger.info('gps sample rate: {0}, call interval {1}, call time: {2}'.format(GPS_fs, call_int, call_time)) # Burst Int and burst time have not been defined yet
+    # logger.info('gps sample rate: {0}, call interval {1}, call time: {2}'.format(GPS_fs, call_int, call_time)) # call_int and call_time have not been defined yet
 
     # Define loop counter
     i = 1
