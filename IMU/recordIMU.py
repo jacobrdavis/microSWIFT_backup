8d111833750c3e971086eb4caf6d7965d8d101c4
EJ Rainville <erainvil@uw.edu>
2021-10-13 07:52:59 -0400

    changed IMU ranges to 4g for accelerations and 1000 dps for gyro

--- IMU/recordIMU.py
+++ IMU/recordIMU.py
@@ -68,8 +68,8 @@ def recordIMU(end_time):
         logger.info('power on IMU')
         GPIO.output(imu_gpio,GPIO.HIGH)
         i2c = busio.I2C(board.SCL, board.SDA)
-        fxos = IMU.adafruit_fxos8700_microSWIFT.FXOS8700(i2c, accel_range=0x00)
-        fxas = IMU.adafruit_fxas21002c_microSWIFT.FXAS21002C(i2c, gyro_range=500)
+        fxos = IMU.adafruit_fxos8700_microSWIFT.FXOS8700(i2c, accel_range=0x01)
+        fxas = IMU.adafruit_fxas21002c_microSWIFT.FXAS21002C(i2c, gyro_range=1000)
 
         # Sleep to start recording at same time as GPS
         sleep(5.1)
